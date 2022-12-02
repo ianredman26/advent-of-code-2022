c943a3a50333f11a4c1f6d34dcb95431da3558be
Ian Redman <ian.redman@thetrainline.com>
2022-12-02 09:36:03 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): List<Pair<Int, Int>> {
+        val elves: MutableMap<Int, Int> = mutableMapOf()
+        var total = 0
+        input.map {
+            total = if (it.isEmpty()) {
+                elves[elves.size.plus(1)] = total
+                0
+            } else {
+                total.plus(it.toInt())
+            }
+        }
+        return elves.toList().sortedByDescending { (_, value) -> value }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Pair<Int, Int>>): Int {
+        val total = input[0].second.plus(input[1].second).plus(input[3].second)
+        return total
     }
+
+    val testInput = readInput("Day01_part1")
+
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+//    check(part1(testInput) == 4)
+
+//    val input = readInput("Day01")
+    println(part1(testInput))
+    println(part2(part1(testInput)))
 }
